14e477a242a742afd9b4833742a336ff24036ee9
Jimmy Ebersold <jimmyebersold@aol.com>
2023-11-27 13:24:17 -0500

    added albas stuff

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -6,14 +6,23 @@ using UnityEngine.Audio;
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public float sensitivity = 10f;
+    //public float volume1;
+    //public float sensitivity;
+    //public void Update()
+    //{
+    //    PlayerStats.volume = volume1;
+    //    PlayerStats.sens = sensitivity;
+    //}
     public void SetVolume(float volume)
     {
-        //Debug.Log(volume);
         audioMixer.SetFloat("volume", volume);
+        PlayerStats.volume = volume;
+
     }
     public void SetSense(float sens)
     {
-        sensitivity = sens;
+        PlayerStats.sens = sens;
+        Debug.Log("player = " + PlayerStats.sens);
+        Debug.Log("sens = " + sens);
     }
 }
